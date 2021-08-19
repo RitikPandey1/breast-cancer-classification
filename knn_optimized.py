13677e339a731e68248f85591b1da83910a1166d
RitikPandey1 <rp18.pandey@gmail.com>
2021-08-19 22:15:47 +0530

    feature scaling

--- knn_optimized.py
+++ knn_optimized.py
@@ -10,6 +10,12 @@ from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+
 from sklearn.neighbors import KNeighborsClassifier
 
 err_rate = []
